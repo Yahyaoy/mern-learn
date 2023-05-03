c210bccff4b709d7086822613f17174e062cfdb7
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-05-04 00:38:05 +0300

    before use router

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5000
 
 
 const app = express()
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message: 'Get goals'})
+})
 
 app.listen(port,()=>console.log(`server start with port ${port}`))
